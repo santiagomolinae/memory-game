cfbbde463dcae9e0f62fb8405ef610c81818a250
Daniel Rodriguez <gameover_lifetime@outlook.com>
2019-07-08 12:59:19 -0500

    Style Change

--- src/utils/BuildDeck.js
+++ src/utils/BuildDeck.js
@@ -1,20 +1,20 @@
-import shuffle from 'lodash.shuffle';
-import FontAwesomeClasses from './FontAwesomeClasses';
-const NUMBER_OF_CARDS = 20;
+import shuffle from 'lodash.shuffle'
+import FontAwesomeClasses from './FontAwesomeClasses'
+const NUMBER_OF_CARDS = 20
 export default () => {
-    const fontAwesomeClasses = FontAwesomeClasses();
-    let cards = [];
+	const fontAwesomeClasses = FontAwesomeClasses()
+	let cards = []
 
-    while(cards.length < NUMBER_OF_CARDS){
-        const index = Math.floor(Math.random() * fontAwesomeClasses.length);
-        const card = {
-            icon: fontAwesomeClasses.splice(index, 1)[0],
-            wasGuessed: false  
-        };
+	while (cards.length < NUMBER_OF_CARDS) {
+		const index = Math.floor(Math.random() * fontAwesomeClasses.length)
+		const card = {
+			icon       : fontAwesomeClasses.splice(index, 1)[0],
+			wasGuessed : false
+		}
 
-        cards.push(card);
-        cards.push({...card});
-    }
+		cards.push(card)
+		cards.push({ ...card })
+	}
 
-    return shuffle(cards);
-};
+	return shuffle(cards)
+}

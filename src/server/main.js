bb8c3d128cc7201ceebdae28033203d55d3cfddd
santiagomolinae <santiago.molinae@udea.edu.co>
2019-07-22 23:26:44 -0500

    server

---
+++ src/server/main.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('src/App.js'));
+
+app.get('/', function(req, res){
+    res.status(200);
+});
+
+io.on('connection', function(socket){
+    console.log("Conected with socket");
+});
+
+server.listen(3000, function(){
+    console.log("Server running on http://localhost:8080");
+});

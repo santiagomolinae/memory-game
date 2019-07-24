a93c2eb2687c32ae868abd7fb05ead8a19980750
Daniel Rodriguez <gameover_lifetime@outlook.com>
2019-07-23 23:47:39 -0500

    Added the rest of functions
    Added the rest of the missing functions and their applications with socket.io

--- Server/index.js
+++ Server/index.js
@@ -13,6 +13,9 @@ var fs = require('fs')
 //  Getting port
 var port = process.env.PORT || 8080
 
+//  Limiting how many games can check before joining
+var loopLimit = 0
+
 server.listen(port, function () {
   console.log('Listening on port: %d', port)
   fs.writeFile(path.join(__dirname, '/start.log'), 'server started', function () {})
@@ -29,10 +32,10 @@ var gameCollection = {
 //  Create new game
 function newGame (socket) {
   var game = {}
-  //  Assing id
+  //  Assign id
   game.id = (Math.random() + 1).toString(36).slice(2, 18)
 
-  //  Assing players
+  //  Assign players
   //  Only player one, player two has to join after
   game.playerOne = socket.username
   game.playerTwo = null
@@ -108,3 +111,131 @@ function deleteGame (socket) {
     socket.emit('notInGame')
   }
 }
+
+//  Seek game
+function gameSeeker (socket) {
+  ++loopLimit
+  //  Check if limit exceeded
+  if (gameCollection.totalGameCount === 0 || loopLimit >= 10) {
+    //  Limit exceeded
+    //  Create new game
+    newGame(socket)
+
+    //  Reset Counter
+    loopLimit = 0
+  } else {
+    //  Limit not exceeded
+    //  Pick random game
+    var rndPick = Math.floor(Math.random() * gameCollection.totalGameCount)
+
+    //  Check if player two can join
+    if (gameCollection.gameList[rndPick]['gameObject']['playerTwo'] == null) {
+      //  Player two can join
+      //  Assign player two to game
+      gameCollection.gameList[rndPick]['gameObject']['playerTwo'] = socket.username
+
+      //  Show player two joined
+      socket.emit('joinSuccess', {
+        gameId: gameCollection.gameList[rndPick]['gameObject']['id']
+      })
+
+      console.log(socket.username + ' has been added to: ' + gameCollection.gameList[rndPick]['gameObject']['id'])
+    } else {
+      //  Player two can't join
+      //  Keep looking
+      gameSeeker(socket)
+    }
+  }
+}
+
+// Chatroom
+var numUsers = 0
+
+io.on('connection', function (socket) {
+  var addedUser = false
+
+  // When the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    })
+  })
+
+  // When the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return
+
+    // we store the username in the socket session for this client
+    socket.username = username
+    ++numUsers
+    addedUser = true
+    socket.emit('login', {
+      numUsers: numUsers
+    })
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    })
+  })
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    })
+  })
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    })
+  })
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers
+      deleteGame(socket)
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      })
+    }
+  })
+
+  socket.on('joinGame', function () {
+    console.log(socket.username + ' wants to join a game')
+
+    var alreadyInGame = false
+
+    for (var i = 0; i < gameCollection.totalGameCount; i++) {
+      var plyr1Tmp = gameCollection.gameList[i]['gameObject']['playerOne']
+      var plyr2Tmp = gameCollection.gameList[i]['gameObject']['playerTwo']
+      if (plyr1Tmp === socket.username || plyr2Tmp === socket.username) {
+        alreadyInGame = true
+        console.log(socket.username + ' already has a Game!')
+
+        socket.emit('alreadyJoined', {
+          gameId: gameCollection.gameList[i]['gameObject']['id']
+        })
+      }
+    }
+    if (alreadyInGame === false) {
+      gameSeeker(socket)
+    }
+  })
+
+  socket.on('leaveGame', function () {
+    if (gameCollection.totalGameCount === 0) {
+      socket.emit('notInGame')
+    } else {
+      deleteGame(socket)
+    }
+  })
+})
